efc035627905b2e9d2b425de7219b0510b2d0048
Jyoti Gupta <joteegupta1.0@gmail.com>
2023-06-23 15:30:44 +0530

    code Refactor

--- gitactionstest/test/Users/test_get_api.py
+++ gitactionstest/test/Users/test_get_api.py
@@ -3,24 +3,24 @@ import json
 import pytest
 import logging as logger
 
-
 base_url = "https://reqres.in/api/users?page=2"
 
 
 @pytest.mark.smoke
+@pytest.mark.tc01
 def test_get_method():
     logger.debug("Get the user data")
     response = requests.get(base_url)
     response.json()
-    json_data=json.dumps((response.json()), indent=4)
+    json_data = json.dumps((response.json()), indent=4)
     print(json_data)
-    assert response.status_code == 201, f"Status code showing wrong"
+    assert response.status_code == 200, f"Status code showing wrong"
+
 
 @pytest.mark.smoke
+@pytest.mark.tc02
 def test_get_new_method():
     logger.debug("Get the user data")
     response = requests.get(base_url)
-    response.json()
-    json_data=json.dumps((response.json()), indent=4)
-    print(json_data)
+    json_data = json.dumps((response.json()), indent=4)
     assert response.status_code == 200, f"Status code showing wrong"
