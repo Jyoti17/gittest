40f272388f806497de29ef4f87b86397cc8c5834
Jyoti Gupta <joteegupta1.0@gmail.com>
2023-06-23 11:05:42 +0530

    third

--- gitactionstest/test/test_get_api.py
+++ gitactionstest/test/test_get_api.py
@@ -1,18 +1,19 @@
 import requests
 import json
 import pytest
+import logging as logger
+
 
 base_url = "https://reqres.in/api/users?page=2"
 
 
 @pytest.mark.tcid011
 def test_get_method():
+    logger.debug("Get the user data")
     response = requests.get(base_url)
     response.json()
     json_data=json.dumps((response.json()), indent=4)
     print(json_data)
-    print("Test Jyoti G code")
-    print("Test Yogesh Gondhali's code")
-
+    assert response.status_code == 200, f"Status code showing wrong"
 
 test_get_method()
